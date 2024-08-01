3ee6b6ddab93ba4ced08f8ebfa57e9ca42b5fa51
fluetanol <etanolfly98@gmail.com>
2024-08-01 18:37:34 +0900

    feat) collide and slide 구현 방식
    오르막, 내리막에서 불안정함

--- Assets/Script/PlayerInput.cs
+++ Assets/Script/PlayerInput.cs
@@ -1,6 +1,6 @@
-
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Interactions;
 
 public class PlayerInput : MonoBehaviour, InputInterface{
 
@@ -31,65 +31,111 @@ public class PlayerInput : MonoBehaviour, InputInterface{
     void OnEnable() => _playerControls.Enable();
     void OnDisable() => _playerControls.Disable();
 
+
     void FixedUpdate(){
          _normalMove = _normalMoveInterface.NormalMovingVector();
         _gravityMove = _fallingInterface.FallingVector();
 
         Vector2 currentPosition = _playerRigidbody.position;
         Vector2 expectPosition = _playerRigidbody.position + _normalMove + _gravityMove;
-        Collision(currentPosition, expectPosition);
 
+        Collision(currentPosition, expectPosition, 3);
         _playerRigidbody.MovePosition(_finalMove);
     }
 
 
-    private void Collision(Vector2 currentPosition, Vector2 expectPosition){
-        RaycastHit2D[] hits = new RaycastHit2D[10];
-        int hitCount = CollisionCast(currentPosition, expectPosition,ref hits);
-        Physics2D.queriesStartInColliders = false;
-        if (hitCount==0){
-            print("air");
-            _fallingInterface.EnableFalling();
-        }
-        else {
-            print("collision");
-            CollsionHitCheck(hits,hitCount,ref currentPosition, ref expectPosition);
+    private void Collision(Vector2 currentPosition, Vector2 expectPosition, int depth){
+        RaycastHit2D[] hits = new RaycastHit2D[1];
+        for (int i=0; i<depth; i++){
+            bool isCast = CollisionCast(currentPosition, expectPosition, ref hits);
+            if (isCast){
+                if(!CollsionHitCheck(hits, ref currentPosition, ref expectPosition)){
+                    break;
+                }
+            }
+            else if(!isCast && i == 0){
+                _fallingInterface.SetGravityDirection(Vector2.down);
+                break;
+            }
+            else break;
         }
         _finalMove = expectPosition;
     }
 
 
-    private int CollisionCast(Vector2 currentPosition, Vector2 expectPosition, ref RaycastHit2D[] hits){
+    private bool CollisionCast(Vector2 currentPosition, Vector2 expectPosition, ref RaycastHit2D[] hits, float skinWidth = 0.015f){
         CapsuleCollider2D collider = GetComponent<CapsuleCollider2D>();
         Vector2 CastDirection = expectPosition - currentPosition;
-        float CastDistance = Vector2.Distance(expectPosition, currentPosition);
+        float CastDistance = CastDirection.magnitude + skinWidth;
+
         int k = collider.Cast(CastDirection, hits, CastDistance, true);
-        return k;
+        return k > 0 ? true : false;
     }
 
-    private void CollsionHitCheck(RaycastHit2D[] hits, int hitCount,ref Vector2 currentPosition, ref Vector2 expectPosition){
-        print(hitCount);
-        for (int i = 0; i < hitCount; i++){
-            RaycastHit2D hit = hits[i];
-            Vector2 normal = hit.normal;
-            float angle = Vector2.SignedAngle(Vector2.up, normal);
+
+    private bool CollsionHitCheck(RaycastHit2D[] hits,ref Vector2 currentPosition, ref Vector2 expectPosition){
+        RaycastHit2D hit = hits[0];
+        Vector2 hitNormal = hit.normal;
+        Vector2 velocity = expectPosition - currentPosition;
+        Vector2 snapSurfaceVector = velocity.normalized * (hit.distance - 0.015f);
         
-            if (angle < 45 && angle > -45){
-                SlopeDirection(normal, ref currentPosition, ref expectPosition, ref hit);
-               _fallingInterface.DisableFalling();
-                break;
-            }
+        // snap surface로 충돌면 까지 가는 벡터를 구하고,
+        // 잔여 속도 벡터의 양만큼 다시 collision casting을 하는 것.
+        Vector2 leftover = velocity - snapSurfaceVector;
+
+       // if (snapSurfaceVector.magnitude <= 0.015f) snapSurfaceVector = Vector2.zero;
+
+        float angle = Vector2.Angle(Vector2.up, hitNormal);
         
+        if (angle <= 45){
+            Vector2 project = ProjectAndScale(leftover, hitNormal);
+            
+            currentPosition = currentPosition + snapSurfaceVector;
+            expectPosition = currentPosition + project;
+
+            SlopeDirection(hitNormal,project, ref expectPosition, ref hit);
+            _fallingInterface.DisableFalling();
         }
+
+   
+        else{
+         /*
+            float scale = 1 - Vector2.Dot(
+                new Vector2(hitNormal.x,0).normalized,
+                -new Vector2(0.1f, 0).normalized
+            ); 
+
+            leftover.y = 0;
+            hitNormal.y = 0;
+            Vector2 project = ProjectAndScale(leftover, hitNormal).normalized
+            * scale;*/
+
+            Vector2 project = ProjectAndScale(leftover, hitNormal);
+            print(project +" " +snapSurfaceVector);
+
+            //expectPosition +=_gravityMove;
+            //expectPosition.x = expectPosition.x + hit.normal.x * hit.distance;
+           // print(expectPosition);
+
+            _fallingInterface.DisableFalling();
+            return false;
+        }
+
+        return true;
+    }
+    
+    private Vector2 ProjectAndScale(Vector2 vec, Vector2 normal){
+        float mag = vec.magnitude;
+        Vector2 project = Vector3.ProjectOnPlane(vec, normal).normalized * mag;
+        return project;
     }
 
-    private void SlopeDirection(Vector2 normal, ref Vector2 currentPosition, ref Vector2 expectPosition, ref RaycastHit2D hit){
-        Vector2 projectVector = Vector3.ProjectOnPlane(_normalMove, normal);
-        Vector2 slopeDirection = projectVector - _normalMove;
+
+
+    private void SlopeDirection(Vector2 normal, Vector2 project, ref Vector2 expectPosition, ref RaycastHit2D hit){
         CapsuleCollider2D collider = GetComponent<CapsuleCollider2D>();
-        expectPosition = hit.point + (normal * collider.size.x / 2) + Vector2.up * (collider.size.y / 2 - collider.size.x/2);
         _fallingInterface.SetGravityDirection(-normal);
-        _normalMoveInterface.SetSlopeDirection(normal);
+       _normalMoveInterface.SetSlopeDirection(normal);
     }
 
     //initialize GetComponent
@@ -117,9 +163,7 @@ public class PlayerInput : MonoBehaviour, InputInterface{
     }
 
     public void OnJump(InputAction.CallbackContext ctx){
-        print("?");
         _jumpMove = Vector2.up * JumpHeight;
     }
 
 }
-
